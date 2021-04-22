3e5996224072bcd3931fc222bb958474b3db3c42
EnRui211825 <82471870+EnRui211825@users.noreply.github.com>
2021-04-22 15:24:33 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,48 @@
+controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
+    mySprite.vx = randint(-9, 9)
+    mySprite.vy = randint(-9, 9)
+})
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    mySprite.setPosition(randint(15, 145), randint(15, 105))
+    mySprite2.setPosition(randint(15, 145), randint(15, 105))
+})
+let mySprite2: Sprite = null
+let mySprite: Sprite = null
+mySprite = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . c c c c . . . . 
+    . . . . . . c c d d d d c . . . 
+    . . . . . c c c c c c d c . . . 
+    . . . . c c 4 4 4 4 d c c . . . 
+    . . . c 4 d 4 4 4 4 4 1 c . c c 
+    . . c 4 4 4 1 4 4 4 4 d 1 c 4 c 
+    . c 4 4 4 4 1 4 4 4 4 4 1 c 4 c 
+    f 4 4 4 4 4 1 4 4 4 4 4 1 4 4 f 
+    f 4 4 4 f 4 1 c c 4 4 4 1 f 4 f 
+    f 4 4 4 4 4 1 4 4 f 4 4 d f 4 f 
+    . f 4 4 4 4 1 c 4 f 4 d f f f f 
+    . . f f 4 d 4 4 f f 4 c f c . . 
+    . . . . f f 4 4 4 4 c d b c . . 
+    . . . . . . f f f f d d d c . . 
+    . . . . . . . . . . c c c . . . 
+    `, SpriteKind.Player)
+mySprite.setPosition(randint(15, 145), randint(15, 105))
+mySprite2 = sprites.create(img`
+    .............ccfff..............
+    ............cddbbf..............
+    ...........cddbbf...............
+    ..........fccbbcf............ccc
+    ....ffffffccccccff.........ccbbc
+    ..ffbbbbbbbbbbbbbcfff.....cdbbc.
+    ffbbbbbbbbbcbcbbbbcccff..cddbbf.
+    fbcbbbbbffbbcbcbbbcccccfffdbbf..
+    fbbb1111ff1bcbcbbbcccccccbbbcf..
+    .fb11111111bbbbbbcccccccccbccf..
+    ..fccc33cc11bbbbccccccccfffbbcf.
+    ...fc131c111bbbcccccbdbc...fbbf.
+    ....f33c111cbbbfdddddcc.....fbbf
+    .....ff1111fbdbbfddcc........fff
+    .......cccccfbdbbfc.............
+    .............fffff..............
+    `, SpriteKind.Player)
+mySprite.setPosition(randint(15, 145), randint(15, 105))
